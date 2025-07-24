8ec69fe6ba742c0747e996260e957808738ad0e8
Kacper Wojciechowski <kacper.wojciechowski08090@gmail.com>
2025-07-24 19:36:42 +0200

    Add inline extension and composable functions for catching Boomerang values without proxy feature

--- compose/src/commonMain/kotlin/io/buszi/boomerang/compose/CatchBoomerangLifecycleEffect.kt
+++ compose/src/commonMain/kotlin/io/buszi/boomerang/compose/CatchBoomerangLifecycleEffect.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LifecycleEventEffect
+import io.buszi.boomerang.core.Boomerang
 import io.buszi.boomerang.core.BoomerangCatcher
 import io.buszi.boomerang.core.assertValidForBoomerangCatcher
 import io.buszi.boomerang.core.eventBoomerangCatcher
@@ -34,9 +35,36 @@ fun CatchBoomerangLifecycleEffect(
 }
 
 /**
+ * A Composable effect that tries to catch a boomerang value from the BoomerangStore when a specific lifecycle event occurs.
+ * This is an inline version that accepts a lambda function instead of a BoomerangCatcher.
+ * This effect is typically used to process data that was stored in the BoomerangStore when the UI becomes visible.
+ * [Example usage](https://github.com/buszi/Boomerang/blob/main/app/src/main/java/io/buszi/boomerang/FullComposePreviewActivity.kt)
+ *
+ * @param key The key to try to catch the value for
+ * @param lifecycleEvent The lifecycle event that triggers the catch attempt (default is ON_START)
+ * @param catcher Lambda function that will be called with the caught Boomerang value
+ * @throws IllegalArgumentException if the lifecycleEvent is not ON_START or ON_RESUME
+ */
+@Composable
+inline fun CatchBoomerangLifecycleEffect(
+    key: String,
+    lifecycleEvent: Lifecycle.Event = Lifecycle.Event.ON_START,
+    crossinline catcher: (Boomerang) -> Unit,
+) {
+    CatchBoomerangLifecycleEffect(
+        key = key,
+        lifecycleEvent = lifecycleEvent,
+        catcher = BoomerangCatcher { boomerang ->
+            catcher(boomerang)
+            true
+        }
+    )
+}
+
+/**
  * A specialized Composable effect for catching event notifications from the BoomerangStore.
  * This is a convenience function that simplifies catching events in Compose UI.
- * 
+ *
  * Unlike the more general CatchBoomerangLifecycleEffect, this function is specifically
  * designed for handling simple event notifications without additional data.
  * [Example usage](https://github.com/buszi/Boomerang/blob/main/app/src/commonMain/kotlin/io/buszi/boomerang/ComposePreviewApp.kt)
--- fragment/src/main/java/io/buszi/boomerang/fragment/CatchBoomerangWithLifecycleEvent.kt
+++ fragment/src/main/java/io/buszi/boomerang/fragment/CatchBoomerangWithLifecycleEvent.kt
@@ -4,6 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
+import io.buszi.boomerang.core.Boomerang
 import io.buszi.boomerang.core.BoomerangCatcher
 import io.buszi.boomerang.core.assertValidForBoomerangCatcher
 import io.buszi.boomerang.core.eventBoomerangCatcher
@@ -45,6 +46,34 @@ fun Fragment.catchBoomerangWithLifecycleEvent(
         }
     }
     lifecycle.addObserver(observer)
+}
+
+/**
+ * Extension function for Fragment that sets up a lifecycle observer to catch a boomerang value when a specific lifecycle event occurs.
+ * This is an inline version that accepts a lambda function instead of a BoomerangCatcher.
+ * This function should be called in the Fragment's onCreate method to not multiply the observers.
+ * [Example usage](https://github.com/buszi/Boomerang/blob/main/app/src/main/java/io/buszi/boomerang/FullFragmentPreviewActivity.kt)
+ *
+ * @param key The key to try to catch the value for
+ * @param lifecycleEvent The lifecycle event that triggers the catch attempt (default is ON_START)
+ * @param catcher Lambda function that will be called with the caught Boomerang value
+ * @throws IllegalArgumentException if the lifecycleEvent is not ON_START or ON_RESUME
+ * @throws IllegalStateException if called after Fragment's onCreate, if the Fragment is not attached to an Activity, 
+ *                              if the Activity does not implement BoomerangStoreHost, or if the BoomerangStore is not initialized
+ */
+inline fun Fragment.catchBoomerangWithLifecycleEvent(
+    key: String,
+    lifecycleEvent: Lifecycle.Event = Lifecycle.Event.ON_START,
+    crossinline catcher: (Boomerang) -> Unit,
+) {
+    catchBoomerangWithLifecycleEvent(
+        key = key,
+        lifecycleEvent = lifecycleEvent,
+        catcher = BoomerangCatcher { boomerang ->
+            catcher(boomerang)
+            true
+        },
+    )
 }
 
 /**
