95b70f9d9207310cb63796a8930c3eea0c34edd1
Kacper Wojciechowski <kacper.wojciechowski08090@gmail.com>
2025-07-25 07:41:44 +0200

    Add `getSerializable` extensions for manually receiving serializable value from BoomerangStore

--- serialization-kotlinx/src/commonMain/kotlin/io/buszi/boomerang/serialization/kotlinx/KotlinxSerializationBoomerangSupport.kt
+++ serialization-kotlinx/src/commonMain/kotlin/io/buszi/boomerang/serialization/kotlinx/KotlinxSerializationBoomerangSupport.kt
@@ -33,6 +33,34 @@ inline fun <reified T : @Serializable Any> BoomerangStore.storeValue(value: T) {
 }
 
 /**
+ * Extension function for [BoomerangStore] that retrieves and deserializes an object of type [T] with the specified key.
+ *
+ * This function retrieves the boomerang with the given key and deserializes it using Kotlinx Serialization.
+ *
+ * @param T The type of serializable object to retrieve
+ * @param key The unique identifier for the stored value
+ * @return The deserialized object of type [T], or null if no value is found for the given key
+ */
+inline fun <reified T : @Serializable Any> BoomerangStore.getSerializable(key: String): T? {
+    val boomerang = getValue(key) ?: return null
+    return BoomerangFormat.deserialize(boomerang)
+}
+
+/**
+ * Extension function for [BoomerangStore] that retrieves and deserializes an object of type [T].
+ *
+ * This function automatically uses the qualified name of type [T] as the key and
+ * retrieves the boomerang with that key, then deserializes it using Kotlinx Serialization.
+ *
+ * @param T The type of serializable object to retrieve
+ * @return The deserialized object of type [T], or null if no value is found for the type's qualified name
+ */
+inline fun <reified T : @Serializable Any> BoomerangStore.getSerializable(): T? {
+    val boomerang = getValue(requireNotNull(T::class.qualifiedName)) ?: return null
+    return BoomerangFormat.deserialize(boomerang)
+}
+
+/**
  * Extension function for [Boomerang] that adds a serializable object of type [T] with the specified key.
  *
  * This function serializes the object using Kotlinx Serialization before adding it to the Boomerang.
